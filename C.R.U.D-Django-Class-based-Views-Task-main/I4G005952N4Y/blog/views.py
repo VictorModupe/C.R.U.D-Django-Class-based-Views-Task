033af9797f68b63110a2d6843147bc9fb690feb5
Victor Modupson <98719622+VictorModupe@users.noreply.github.com>
2022-06-25 17:25:05 +0100

    Push that was made to Github Initially

---
+++ C.R.U.D-Django-Class-based-Views-Task-main/I4G005952N4Y/blog/views.py
@@ -0,0 +1,28 @@
+from msilib.schema import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.views.generic.detail import DetailView
+from .models import Post
+from django.urls import reverse_lazy
+
+class PostListView(ListView):
+ model = Post
+
+class PostCreateView(CreateView):
+ model = Post
+ fields = "__all__"
+ success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+ model = Post
+
+class PostUpdteView(UpdateView):
+ model = Post
+ fields = "__all__"
+ success_url  = reverse_lazy("blog:all")
+ 
+class PostDeleteView(DeleteView):
+ model = Post
+ fields = "__all__"
+ success_url  = reverse_lazy("blog:all")
---
+++ C.R.U.D-Django-Class-based-Views-Task-main/README.md
@@ -0,0 +1,2 @@
+# C.R.U.D-Django-Class-based-Views-Task
+Django Task for I4G005952N4Y
